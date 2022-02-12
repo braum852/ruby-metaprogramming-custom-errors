d6942619312e1840e872759885c51911ef653b04
braum852 <julian.teo59@yahoo.com>
2022-02-12 13:10:10 +1000

    Done

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,14 +7,28 @@ class Person
 
   def get_married(person)
     self.partner = person
+    if person.class != Person
+      begin
+      raise PartnerError
+      rescue PartnerError => error
+        puts error.message
+      end
+    else
     person.partner = self
   end
+end
 
+class PartnerError < StandardError
+  def message
+    "No instance for the get_married method in the person class, no worky worky"
+  end
+end
 end
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
+
 
 
 
